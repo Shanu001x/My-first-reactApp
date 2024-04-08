6c2c35b49fb65c1bf5304a3e3ef03236292c5b48
Shanu001x <sraw419@gmail.com>
2024-04-08 18:08:10 +0530

    working on A full blog website project

--- MegaProject/src/App.jsx
+++ MegaProject/src/App.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login,logout} from "./store/authSlice.js"
+import {Header, Footer} from "./components/index.js"
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-   <h1>
-    A blog With Appwrite
-   </h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if(userData) {
+        dispatch(login(userData))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{setLoading(false)})
+  },[])
+
+  return !loading? (
+    <div className='min-height-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         Todo: {/*outlet*/}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
